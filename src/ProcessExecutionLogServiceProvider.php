60680ec58d4434013203ebeea40690d58089995e
Wouter <wouter@Wouters-MacBook.local>
2023-07-18 21:14:32 +0200

    Added Service Provider + Updated composer.json

---
+++ src/ProcessExecutionLogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FerdinandosCo\ProcessExecutionLog;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProcessExecutionLogServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //...
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //...
+    }
+}
